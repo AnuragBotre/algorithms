079afc04eb8e0a9e6a3bd647bbf21d2a3b7b6503
Anurag <anurag.botre.202@gmail.colm>
2017-11-26 22:09:52 +0530

    Combination of Characters using recursion.

--- JDK18Learning/src/main/java/com/trendcore/problems/ForLoopUsingRecursion.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/ForLoopUsingRecursion.java
@@ -13,22 +13,49 @@ public class ForLoopUsingRecursion {
 
         //generate sequence of how much length
 
+        /**
+         * Recursion is correct.
+         */
+
 
         //loopUsingRecursion.recurse(5);
-        loopUsingRecursion.printInputs(new int[]{1, 2}, 3);
+        loopUsingRecursion.printInputs(new int[]{1, 2}, 0, 0, 3, "");
     }
 
-    private void printInputs(int[] ints, int length) {
-        for(int i = 0 ; i < ints.length ; i++){
-            System.out.print(ints[i]);
-            printCombinations(i,ints,length-1);
-            System.out.println();
+
+//    private String s = "";
+
+    private void printInputs(int[] ints, int arrayIndex, int position, int length, String s) {
+
+        if (arrayIndex >= ints.length) {
+            return;
         }
+
+        //String s = "";
+        printChars(ints, s, arrayIndex, position, length);
+        printInputs(ints,arrayIndex+1,position,length,s);
     }
 
-    private void printCombinations(int arrayIndex, int[] ints, int length) {
-        for(int i=0;i<length;i++){
-            printInputs(ints,length);
+    private void printChars(int[] ints, String s, int arrayIndex, int position, int length) {
+
+        if (position == length) {
+            return;
         }
+        s = s + ints[arrayIndex];
+        printChars(ints, s, arrayIndex, position + 1, length);
+        if (s.length() == length) {
+            System.out.println(s);
+        }
+        s = s.substring(0, s.length() - 1);
+        printInputsOther(ints, arrayIndex + 1, position, length, s);
+    }
+
+    private void printInputsOther(int[] ints, int arrayIndex, int position, int length, String s) {
+        if (arrayIndex >= ints.length) {
+            return;
+        }
+
+        //String s = "";
+        printChars(ints, s, arrayIndex, position, length);
     }
 }
