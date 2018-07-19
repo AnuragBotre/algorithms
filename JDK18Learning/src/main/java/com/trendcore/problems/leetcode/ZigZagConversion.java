fc37ea106e9f435908768c962438c577e5491f73
anurag <anurag.botre.202@gmail.com>
2018-07-19 12:27:18 +0530

    ZigZag Conversion Solution

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ZigZagConversion.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ZigZagConversion.java
@@ -6,79 +6,95 @@ import java.util.List;
 /**
  * https://leetcode.com/problems/zigzag-conversion/description/
  * 6. ZigZag Conversion
- *
- The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
-
- P   A   H   N
- A P L S I I G
- Y   I   R
- And then read line by line: "PAHNAPLSIIGYIR"
-
- Write the code that will take a string and make this conversion given a number of rows:
-
- string convert(string s, int numRows);
- Example 1:
-
- Input: s = "PAYPALISHIRING", numRows = 3
- Output: "PAHNAPLSIIGYIR"
- Example 2:
-
- Input: s = "PAYPALISHIRING", numRows = 4
- Output: "PINALSIGYAHRPI"
- Explanation:
-
- P     I    N
- A   L S  I G
- Y A   H R
- P     I
+ * <p>
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ * <p>
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * <p>
+ * Write the code that will take a string and make this conversion given a number of rows:
+ * <p>
+ * string convert(string s, int numRows);
+ * Example 1:
+ * <p>
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * Example 2:
+ * <p>
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * <p>
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
  */
 public class ZigZagConversion {
 
     public static void main(String[] args) {
         ZigZagConversion z = new ZigZagConversion();
-        System.out.println(z.convert("PAYPALISHIRING",4));
+        System.out.println(z.convert("PAYPALISHIRING", 4));
+        System.out.println(z.convert("", 1));
+        System.out.println(z.convert("A", 2));
     }
 
     public String convert(String s, int numRows) {
 
-        int j=0;
-        List c[]=new List[numRows];
+        for(int i=0 ; i < s.length() ; i++){
 
-        boolean zig=true;
+        }
 
-        String r1="";
+        return "";
+    }
 
-        for(int i = 0 ; i < s.length() ; i++){
+    private String approach1(String s, int numRows) {
+        if(s.length() == 0)
+            return "";
 
-            if(zig) {
+        int j = 0;
+        String c[] = new String[numRows];
+
+        for(int i = 0 ; i < c.length ; i++){
+            c[i] = "";
+        }
+
+        boolean zig = true;
+
+        String r1 = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (zig) {
                 int a;
-                for (j = 0 ,a=i; j < numRows && a < s.length(); a++,j++) {
-                    if(c[j] == null){
-                        c[j] = new ArrayList();
+                for (j = 0, a = i; j < numRows && a < s.length(); a++, j++) {
+                    if (c[j] == null) {
+                        c[j] = "";
                     }
-                    c[j].add(s.charAt(a));
+                    //c[j].add(s.charAt(a));
+                    c[j] = c[j] + s.charAt(a);
                     //System.out.println(s.charAt(j));
                 }
                 zig = false;
-                i=a-1;
+                i = a - 1;
                 j--;
-            }else{
+            } else {
                 j--;
-                if(j<=0){
+                if (j <= 0) {
                     i--;
-                    zig=true;
+                    zig = true;
                     continue;
                 }
-                c[j].add(s.charAt(i));
 
+                c[j] = c[j] + s.charAt(i);
             }
         }
 
         String r = "";
-        for(int i = 0 ; i < c.length ; i++){
-            for(int k = 0 ; k < c[i].size() ; k++){
-                r = r + c[i].get(k);
-            }
+        for (int i = 0; i < c.length; i++) {
+            r = r + c[i];
         }
 
         return r;
