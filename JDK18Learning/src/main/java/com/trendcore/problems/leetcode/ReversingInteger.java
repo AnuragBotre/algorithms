1797590a247bd8d8035ee0a9047c1e989febdaae
anurag <anurag.botre.202@gmail.com>
2018-07-19 14:52:43 +0530

    Reverse Integer

---
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ReversingInteger.java
@@ -0,0 +1,37 @@
+package com.trendcore.problems.leetcode;
+
+/**
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ * 7. Reverse Integer
+ DescriptionHintsSubmissionsDiscussSolution
+ Given a 32-bit signed integer, reverse digits of an integer.
+
+ Example 1:
+
+ Input: 123
+ Output: 321
+ Example 2:
+
+ Input: -123
+ Output: -321
+ Example 3:
+
+ Input: 120
+ Output: 21
+ Note:
+ Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
+ For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
+public class ReversingInteger {
+
+    public static void main(String[] args) {
+        ReversingInteger r = new ReversingInteger();
+        r.reverse(123);
+    }
+
+    public int reverse(int x) {
+        return 0;
+    }
+
+}
