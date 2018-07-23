fb899a845eb2cd60b2ff810419328354eb9cbf4e
anurag <anurag.botre.202@gmail.com>
2018-07-23 18:16:35 +0530

    Regular Expression Matching.

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/RegularExpressionMatching.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/RegularExpressionMatching.java
@@ -3,60 +3,88 @@ package com.trendcore.problems.leetcode;
 /**
  * https://leetcode.com/problems/regular-expression-matching/description/
  * 10. Regular Expression Matching
- Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
-
- '.' Matches any single character.
- '*' Matches zero or more of the preceding element.
- The matching should cover the entire input string (not partial).
-
- Note:
-
- s could be empty and contains only lowercase letters a-z.
- p could be empty and contains only lowercase letters a-z, and characters like . or *.
- Example 1:
-
- Input:
- s = "aa"
- p = "a"
- Output: false
- Explanation: "a" does not match the entire string "aa".
- Example 2:
-
- Input:
- s = "aa"
- p = "a*"
- Output: true
- Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
- Example 3:
-
- Input:
- s = "ab"
- p = ".*"
- Output: true
- Explanation: ".*" means "zero or more (*) of any character (.)".
- Example 4:
-
- Input:
- s = "aab"
- p = "c*a*b"
- Output: true
- Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
- Example 5:
-
- Input:
- s = "mississippi"
- p = "mis*is*p*."
- Output: false
-
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ * <p>
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * Note:
+ * <p>
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * Example 1:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * Example 2:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * Example 3:
+ * <p>
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * <p>
+ * <p>
+ * Example 4:
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+ * <p>
+ * <p>
+ * <p>
+ * Example 5:
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
  */
 public class RegularExpressionMatching {
 
     public static void main(String[] args) {
         RegularExpressionMatching r = new RegularExpressionMatching();
-        System.out.println(r.isMatch("",""));
+        System.out.println(r.isMatch("aaa", "a"));
+        System.out.println(r.isMatch("aaa", "aa."));
     }
 
     public boolean isMatch(String s, String p) {
-        return false;
+
+        int stringPointer = 0;
+        int patternPointer = 0;
+
+        boolean flag = true;
+
+        for (; flag; ) {
+
+            if ((stringPointer == s.length() && patternPointer == p.length()) || (stringPointer == s.length() && patternPointer == p.length() - 1)) {
+                return true;
+            } else if (stringPointer < s.length() && patternPointer == p.length()) {
+                return false;
+            }
+
+            if (s.charAt(stringPointer) == p.charAt(patternPointer) && (p.charAt(patternPointer) != '.' || p.charAt(patternPointer) != '*')) {
+                patternPointer++;
+            } else if (p.charAt(patternPointer) == '.') {
+                patternPointer++;
+            }else if(p.charAt(patternPointer) == '*'){
+
+            }
+
+
+            stringPointer++;
+        }
+        return true;
     }
 }
