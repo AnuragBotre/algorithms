2f688cb164276ae91c92579b4bcd35524a99b27c
anurag <anurag.botre.202@gmail.com>
2018-08-07 15:14:22 +0530

    Three sum Brute Force Approach

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ThreeSum.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ThreeSum.java
@@ -1,35 +1,85 @@
 package com.trendcore.problems.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * https://leetcode.com/problems/3sum/description/
- *
+ * <p>
  * 15. 3Sum
- Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
-
- Note:
-
- The solution set must not contain duplicate triplets.
-
- Example:
-
- Given array nums = [-1, 0, 1, 2, -1, -4],
-
- A solution set is:
- [
- [-1, 0, 1],
- [-1, -1, 2]
- ]
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate triplets.
+ * <p>
+ * Example:
+ * <p>
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * <p>
+ * A solution set is:
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
  */
 public class ThreeSum {
 
     public static void main(String[] args) {
         ThreeSum t = new ThreeSum();
-        t.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        System.out.println(t.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-        return null;
+
+        return bruteForce(nums);
+    }
+
+    private List<List<Integer>> bruteForce(int[] nums) {
+        int firstPointer = 0;
+        int secondPointer = 0;
+
+        List mainList = new ArrayList();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List list = new ArrayList();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        if (!containsTriplet(mainList, list)) {
+                            mainList.add(list);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return mainList;
+    }
+
+    private boolean containsTriplet(List<List<Integer>> mainList, List<Integer> list) {
+        for(List elements : mainList){
+            if(isEqual(elements,list)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isEqual(List<Integer> elements, List<Integer> list) {
+
+        for(Integer e : elements){
+            if(list.contains(e)){
+                continue;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
     }
 }
