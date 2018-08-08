d028200ec47d3e90cae4c7e335620bcd32bbe825
anurag <anurag.botre.202@gmail.com>
2018-08-08 11:49:10 +0530

    Three sum Modifications to brute force.

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ThreeSum.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ThreeSum.java
@@ -58,8 +58,8 @@ public class ThreeSum {
             }
         }
 
-        //return bruteForce(nums);
-        return mainList;
+        return bruteForce(nums);
+        //return mainList;
     }
 
     private List<List<Integer>> bruteForce(int[] nums) {
@@ -68,8 +68,8 @@ public class ThreeSum {
 
         List mainList = new ArrayList();
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     if (nums[i] + nums[j] + nums[k] == 0) {
                         List list = new ArrayList();
