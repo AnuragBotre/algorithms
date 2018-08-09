35896dc8aaa7d281361b89cbc7f11b1ae8327a95
anurag <anurag.botre.202@gmail.com>
2018-08-09 14:30:57 +0530

    Three sum Approach-1

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ThreeSum.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ThreeSum.java
@@ -28,50 +28,46 @@ public class ThreeSum {
 
     public static void main(String[] args) {
         ThreeSum t = new ThreeSum();
-        System.out.println(t.bruteForce(new int[]{-1, 0, 1, 2, -1, -4}));
-        System.out.println(t.bruteForce(new int[]{5, 4, 9}));
-        System.out.println(t.bruteForce(new int[]{5, 4, 9, -9, 0}));
-        System.out.println(t.bruteForce(new int[]{0,3,0,1,1,-1,-5,-5,3,-3,-3,0}));
-        System.out.println(t.bruteForce(new int[]{-7,-11,12,-15,14,4,4,11,-11,2,-8,5,8,14,0,3,2,3,-3,-15,-2,3,6,1,2,8,-5,-7,3,1,8,11,-3,6,3,-4,-13,-15,14,-8,2,-8,4,-13,13,11,5,0,0,9,-8,5,-2,14,-9,-15,-1,-6,-15,9,10,9,-2,-8,-8,-14,-5,-14,-14,-6,-15,-5,-7,5,-11,14,-7,2,-9,0,-4,-1,-9,9,-10,-11,1,-4,-2,2,-9,-15,-12,-4,-8,-5,-11,-6,-4,-9,-4,-3,-7,4,9,-2,-5,-13,7,2,-5,-12,-14,1,13,-9,-3,-9,2,3,8,0,3}));
+        System.out.println(t.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(t.threeSum(new int[]{5, 4, 9}));
+        System.out.println(t.threeSum(new int[]{5, 4, 9, -9, 0}));
+        System.out.println(t.threeSum(new int[]{0, 3, 0, 1, 1, -1, -5, -5, 3, -3, -3, 0}));
+        //System.out.println(t.threeSum(new int[]{-7, -11, 12, -15, 14, 4, 4, 11, -11, 2, -8, 5, 8, 14, 0, 3, 2, 3, -3, -15, -2, 3, 6, 1, 2, 8, -5, -7, 3, 1, 8, 11, -3, 6, 3, -4, -13, -15, 14, -8, 2, -8, 4, -13, 13, 11, 5, 0, 0, 9, -8, 5, -2, 14, -9, -15, -1, -6, -15, 9, 10, 9, -2, -8, -8, -14, -5, -14, -14, -6, -15, -5, -7, 5, -11, 14, -7, 2, -9, 0, -4, -1, -9, 9, -10, -11, 1, -4, -2, 2, -9, -15, -12, -4, -8, -5, -11, -6, -4, -9, -4, -3, -7, 4, 9, -2, -5, -13, 7, 2, -5, -12, -14, 1, 13, -9, -3, -9, 2, 3, 8, 0, 3}));
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
 
-        //using 2 pointer and hashmap
-        Arrays.sort(nums);
-        Map map = new HashMap();
+        Map additionOf2Num = new HashMap();
+        Map numMap = new HashMap();
 
         for (int i = 0; i < nums.length - 1; i++) {
-            int r = nums[i] + nums[i + 1];
-            if (r <= 0) {
-                //put this in negative no
-                if(map.containsKey(r)){
-                    List o = (List) map.get(r);
-                    List tt = new ArrayList();
-                    tt.add(nums[i]);
-                    tt.add(nums[i+1]);
-                    o.add(tt);
-                }else{
-                    List o = new ArrayList();
-                    List tt = new ArrayList();
-                    tt.add(nums[i]);
-                    tt.add(nums[i+1]);
-                    o.add(tt);
-                    map.put(r,o);
-                }
-            } else {
-                //put this in positive array
-
-                int result = 0 - nums[i];
-                if (map.containsKey(result)) {
-                    System.out.println(nums[i] + " " + map.get(result));
+            for (int j = i + 1; j < nums.length; j++) {
+                int result = nums[i] + nums[j];
+                List l = (List) additionOf2Num.get(result);
+                if (l == null) {
+                    l = new ArrayList();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    additionOf2Num.put(result, l);
                 }
             }
+
+            numMap.put(nums[i],0);
         }
 
-        return bruteForce(nums);
-        //return mainList;
-        //return null;
+        numMap.put(nums[nums.length-1],0);
+
+        for(Object e : additionOf2Num.entrySet()){
+            Map.Entry entry = (Map.Entry) e;
+            int r = 0 - (Integer)entry.getKey();
+            if(numMap.get(r) != null){
+                System.out.println(r + " " + entry.getValue());
+            }
+
+        }
+
+        //return bruteForce(nums);
+        return null;
     }
 
     private List<List<Integer>> bruteForce(int[] nums) {
@@ -113,7 +109,7 @@ public class ThreeSum {
 
         for (Integer e : elements) {
             if (list.contains(e)) {
-                list.remove((Object)e);
+                list.remove((Object) e);
                 continue;
             } else {
                 return false;
