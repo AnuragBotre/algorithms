42d1e713b45143e34fe9369116e302a393d67c26
anurag <anurag.botre.202@gmail.com>
2018-08-09 23:15:42 +0530

    Problem no 16. 3 Sum Closest

---
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/ThreeSumClosest.java
@@ -0,0 +1,26 @@
+package com.trendcore.problems.leetcode;
+
+/**
+ * https://leetcode.com/problems/3sum-closest/description/
+ * 16. 3Sum Closest
+ DescriptionHintsSubmissionsDiscussSolution
+ Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+ Example:
+
+ Given array nums = [-1, 2, 1, -4], and target = 1.
+
+ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+public class ThreeSumClosest {
+
+    public static void main(String[] args) {
+        ThreeSumClosest t = new ThreeSumClosest();
+        t.threeSumClosest(new int[]{-1, 2, 1, -4},1);
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        return 0;
+    }
+
+}
