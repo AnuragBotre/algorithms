f7e3f5f2655d0ed945f10c0f002736a758770269
anurag <anurag.botre.202@gmail.com>
2018-08-12 23:25:46 +0530

    704. Binary Search

---
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/BinarySearch.java
@@ -0,0 +1,30 @@
+package com.trendcore.problems.leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-search/description/
+ * 704. Binary Search
+ * <p>
+ * Given a sorted (in ascending order) integer array nums of n elements and a target value,
+ * write a function to search target in nums.
+ * If target exists, then return its index, otherwise return -1.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+    }
+
+}
