619a96fcace3caf34d97ebbd297f68a9501f903b
anurag <anurag.botre.202@gmail.com>
2018-08-16 14:47:09 +0530

    Combination of Phone numbers.

---
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/CombinationOfPhoneNumbers.java
@@ -0,0 +1,16 @@
+package com.trendcore.problems.leetcode;
+
+import java.util.List;
+
+public class CombinationOfPhoneNumbers {
+
+    public static void main(String[] args) {
+        CombinationOfPhoneNumbers c = new CombinationOfPhoneNumbers();
+        c.letterCombinations("23");
+    }
+
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
+
+}
