2f8e77be46fe785c1b0f673b37f31c195d8791b7
anurag <anurag.botre.202@gmail.com>
2018-08-16 14:48:14 +0530

    Combination of Phone numbers.

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/CombinationOfPhoneNumbers.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/CombinationOfPhoneNumbers.java
@@ -2,6 +2,18 @@ package com.trendcore.problems.leetcode;
 
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ * <p>
+ * 17. Letter Combinations of a Phone Number
+ * <p>
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+ * <p>
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ * <p>
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ */
 public class CombinationOfPhoneNumbers {
 
     public static void main(String[] args) {
