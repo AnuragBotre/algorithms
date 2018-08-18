de742054593d0599a170803a4a90fc07262ec225
anurag <anurag.botre.202@gmail.com>
2018-08-19 01:44:57 +0530

    Four Sum https://leetcode.com/problems/4sum/description/

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/FourSum.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/FourSum.java
@@ -1,5 +1,7 @@
 package com.trendcore.problems.leetcode;
 
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/4sum/description/
  * Given an array nums of n integers and an integer target,
@@ -25,8 +27,23 @@ package com.trendcore.problems.leetcode;
 public class FourSum {
 
     public static void main(String[] args) {
-
+        FourSum f = new FourSum();
+        f.fourSum(new int[]{1, 0, -1, 0, -2, 2},0);
     }
 
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        //Approach 1 Brute Force 4 for loops
+        //Approach 2 Modified Approach 1 3 for loops and store result in the hashmap
+
+        //Approach 3 bifurcate array between positive array and negative array
+        //then sort both array
+        //start with negative array and try to find integers in the positive array
+
+        //Approach 4 sort array go with each no and try to find
+        //remaing 3 look ThreeSumClosest approach that needs to be implemented.
+        //Or complete that example with this approach
+
+        return null;
+    }
 
 }
