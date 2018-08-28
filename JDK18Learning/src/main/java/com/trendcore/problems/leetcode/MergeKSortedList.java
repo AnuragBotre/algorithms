b1761d6213f53e7fe22daafa9e9a5629187c5483
anurag <anurag.botre.202@gmail.com>
2018-08-28 14:35:29 +0530

    https://leetcode.com/problems/merge-k-sorted-lists/description/ 23. Merge k Sorted Lists Implemented Brute Force Approach
    Links
    - Problems for Java 8 practice.

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/MergeKSortedList.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/MergeKSortedList.java
@@ -20,9 +20,36 @@ public class MergeKSortedList {
     public static void main(String[] args) {
         MergeKSortedList m = new MergeKSortedList();
 
-        ListNode[] lists = new ListNode[10];
+        ListNode l1 = m.new ListNode(1);
+        l1.next = m.new ListNode(4);
+        l1.next.next = m.new ListNode(5);
 
-        m.mergeKLists(lists);
+        ListNode l2 = m.new ListNode(1);
+        l2.next = m.new ListNode(3);
+        l2.next.next = m.new ListNode(4);
+
+        ListNode l3 = m.new ListNode(2);
+        l3.next = m.new ListNode(6);
+
+
+        print(m.merge(l1, l2));
+        print(m.merge(l1,null));
+        print(m.merge(null,l1));
+
+        ListNode[] lists = new ListNode[3];
+        lists[0] = l1;
+        lists[1] = l2;
+        lists[2] = l3;
+        print(m.mergeKLists(lists));
+    }
+
+    private static void print(ListNode merge) {
+        System.out.println();
+        ListNode l = merge;
+        while(l != null){
+            System.out.print(" " + l.val);
+            l = l.next;
+        }
     }
 
     public class ListNode {
@@ -35,7 +62,49 @@ public class MergeKSortedList {
     }
 
     public ListNode mergeKLists(ListNode[] lists) {
-        return null;
+        //brute force
+        return bruteForce(lists);
+
+    }
+
+    private ListNode bruteForce(ListNode[] lists) {
+
+        if(lists.length == 0)
+            return null;
+
+        ListNode mergedList = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            mergedList = merge(mergedList, lists[i]);
+        }
+
+        return mergedList;
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode mergeList;
+        ListNode mergeListRoot = mergeList = new ListNode(0);
+        while (list1 != null || list2 != null) {
+            if(list1 != null && list2 != null){
+                if(list1.val < list2.val){
+                    mergeList.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }else{
+                    mergeList.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+            }else{
+                if(list1 != null && list2 == null){
+                    mergeList.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }else if(list1 == null && list2 != null){
+                    mergeList.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+            }
+            mergeList = mergeList.next;
+        }
+        mergeListRoot = mergeListRoot.next;
+        return mergeListRoot;
     }
 
 }
