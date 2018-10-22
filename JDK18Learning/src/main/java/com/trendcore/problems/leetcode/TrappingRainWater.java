f615d807f903843f44a02f7d63bc74b954bee726
anurag <anurag.botre.202@gmail.com>
2018-10-23 00:19:40 +0530

    https://leetcode.com/problems/trapping-rain-water/
    42. Trapping Rain Water

---
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/TrappingRainWater.java
@@ -0,0 +1,57 @@
+package com.trendcore.problems.leetcode;
+
+/**
+ * https://leetcode.com/problems/trapping-rain-water/
+ * <p>
+ * Given n non-negative integers representing an elevation map where
+ * the width of each bar is 1, compute how much water it is able to trap after raining.
+ * <p>
+ * <p>
+ * The above elevation map is represented by
+ * array [0,1,0,2,1,0,1,3,2,1,2,1].
+ * In this case, 6 units of rain water (blue section) are being trapped.
+ *
+ *
+ * Thanks Marcos for contributing this image!
+ * <p>
+ * Example:
+ * <p>
+ * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ */
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+        TrappingRainWater t = new TrappingRainWater();
+        t.testCase(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
+        t.testCase(new int[]{1, 0, 1});
+        t.testCase(new int[]{1, 1, 1});
+        t.testCase(new int[]{1, 2, 1});
+        t.testCase(new int[]{1, 2, 3});
+        t.testCase(new int[]{2, 2, 3});
+        t.testCase(new int[]{2, 0, 3});
+    }
+
+    private void testCase(int[] height) {
+        System.out.println(trap(height));
+    }
+
+    public int trap(int[] height) {
+
+        int startPointer = 0;
+        boolean starPointerInit = false;
+
+        int endPointer = 0;
+
+        for (int i = 0; i < height.length - 1; i++) {
+            startPointer = i;
+            if (!starPointerInit) {
+                //get start pointer
+            }
+        }
+
+
+        return 0;
+    }
+
+}
