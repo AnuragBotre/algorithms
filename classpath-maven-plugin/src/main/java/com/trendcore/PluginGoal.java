db506ae062df76c5cdd2b63401ca6bf95e184142
anurag <anurag.botre.202@gmail.com>
2018-10-23 12:30:17 +0530

    Maven Plugin parameter.

--- classpath-maven-plugin/src/main/java/com/trendcore/PluginGoal.java
+++ classpath-maven-plugin/src/main/java/com/trendcore/PluginGoal.java
@@ -4,12 +4,16 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "classpath")
 public class PluginGoal extends AbstractMojo {
 
+    @Parameter(name = "warPath")
+    private String warPath;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("Executing Goal....");
+        System.out.println("Executing Goal...." + warPath);
     }
 }
