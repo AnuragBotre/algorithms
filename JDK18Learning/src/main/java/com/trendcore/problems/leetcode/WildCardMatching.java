f101402c707dad276c04963442d9a5812fc4ecce
anurag <anurag.botre.202@gmail.com>
2018-10-25 11:18:29 +0530

    https://leetcode.com/problems/wildcard-matching/
    44. Wild Card Matching
    - Partial Solution Implemented

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/WildCardMatching.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/WildCardMatching.java
@@ -52,7 +52,83 @@ package com.trendcore.problems.leetcode;
 public class WildCardMatching {
 
     public static void main(String[] args) {
+        WildCardMatching w = new WildCardMatching();
+        System.out.println(w.isMatch("abc", "*"));
+        System.out.println(w.isMatch("abc", "**"));
+        System.out.println(w.isMatch("abc", "***"));
+        System.out.println(w.isMatch("abc", "abc"));
+        System.out.println(w.isMatch("abc", "???"));
+        System.out.println(w.isMatch("abc", "a"));
+        System.out.println(w.isMatch("abc", "a*c"));
+        System.out.println(w.isMatch("abc", "a?c"));
+        System.out.println(w.isMatch("abc", "a**c"));
+        System.out.println(w.isMatch("abc", "**c"));
+        System.out.println(w.isMatch("abc", "*b*c"));
+        System.out.println(w.isMatch("", "*"));
+        System.out.println(w.isMatch("acdcb", "a*c?b"));
+        System.out.println(w.isMatch("aaaabaaaabbbbaabbbaabbaababbabbaaaababaaabbbbbbaabbbabababbaaabaabaaaaaabbaabbbbaababbababaabbbaababbbba", "*****b*aba***babaa*bbaba***a*aaba*b*aa**a*b**ba***a*a*"));
+    }
 
+    public boolean isMatch(String s, String p) {
+        if (s.length() == 0 && p.length() == 0) {
+            return true;
+        }
+
+        return traverse(s, 0, p, 0);
+    }
+
+    private boolean traverse(String s, int stringPointer, String p, int patternPointer) {
+
+        if (stringPointer == s.length() - 1 && patternPointer == p.length() - 1) {
+            char c = s.charAt(stringPointer);
+            char pc = p.charAt(patternPointer);
+
+            if (c == pc) {
+                return true;
+            } else if (pc == '*' || pc == '?') {
+                return true;
+            }
+
+            return false;
+        }
+
+        if (patternPointer >= p.length()) {
+            return false;
+        }
+
+        if (stringPointer >= s.length()) {
+            //there are no * character left
+            for (int i = patternPointer; i < p.length(); i++) {
+                if (p.charAt(i) != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        char c = p.charAt(patternPointer);
+
+        if (c == '*') {
+
+            //TODO Need to fix this.
+
+            //rule 1
+            boolean traverse = traverse(s, stringPointer + 1, p, patternPointer);
+            if (!traverse) {
+                traverse = traverse(s, stringPointer + 1, p, patternPointer + 1);
+                /*if (!traverse) {
+                    traverse = traverse(s, stringPointer, p, patternPointer + 1);
+                }*/
+            }
+            return traverse;
+        } else if (c == '?') {
+            return traverse(s, stringPointer + 1, p, patternPointer + 1);
+        } else if (c == s.charAt(stringPointer)) {
+            return traverse(s, stringPointer + 1, p, patternPointer + 1);
+        }
+
+        return false;
     }
 
 }
