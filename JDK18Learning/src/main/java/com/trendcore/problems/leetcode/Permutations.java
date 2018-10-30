74faa3b7f537fca9c5fc9c074813459ef53599d9
anurag <anurag.botre.202@gmail.com>
2018-10-30 21:35:21 +0530

    https://leetcode.com/problems/permutations/
    - Initial Commit.

---
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/Permutations.java
@@ -0,0 +1,36 @@
+package com.trendcore.problems.leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/permutations/
+ * Given a collection of distinct integers, return all possible permutations.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [1,2,3]
+ * Output:
+ * [
+ * [1,2,3],
+ * [1,3,2],
+ * [2,1,3],
+ * [2,3,1],
+ * [3,1,2],
+ * [3,2,1]
+ * ]
+ */
+public class Permutations {
+
+    public static void main(String[] args) {
+        Permutations p = new Permutations();
+        p.testCase(new int[]{1,2,3});
+    }
+
+    private void testCase(int[] ints) {
+
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+}
