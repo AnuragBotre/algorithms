5f5caaf1ef9396cf671b7924f3579641a697399d
anurag <anurag.botre.202@gmail.com>
2018-10-31 11:38:29 +0530

    https://leetcode.com/problems/permutations/ 46. permutations
    - Combination of numbers using recursive algo and without using extra space.

--- JDK18Learning/src/main/java/com/trendcore/problems/learning/CombinationOfNumbers.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/learning/CombinationOfNumbers.java
@@ -6,136 +6,54 @@ import java.util.stream.Collectors;
 public class CombinationOfNumbers {
 
     public static void main(String[] args) {
-        CombinationOfNumbers c = new CombinationOfNumbers();
-
-        Set<String> collector = new TreeSet<>();
-
-        c.printCombinations(4, collector);
-
-        collector.forEach(System.out::println);
+        CombinationOfNumbers p = new CombinationOfNumbers();
+        p.testCase(new int[]{1, 2, 3});
     }
 
-    private void printCombinations(int noOfCombinations, Set<String> collector) {
-        String s = "";
-        for (int i = 0; i < noOfCombinations; i++) {
-            s = i + ",";
-            doForRemaining(s, noOfCombinations, collector, noOfCombinations - 1);
-        }
+    private void testCase(int[] ints) {
+        permute(ints);
     }
 
-    private void doForRemaining(String s, int noOfCombinations, Set<String> collector, int rotationalIndex) {
-        if (s.split(",").length == noOfCombinations) {
-            System.out.println(s);
-            //Hash codes are not getting same.
-            //collector.add(s);
+    public List<List<Integer>> permute(int[] nums) {
+        traverse(nums, 0);
+        return null;
+    }
+
+    private void traverse(int[] nums, int i) {
+
+        if (i >= nums.length) {
+            System.out.println();
             return;
         }
 
-        for (int i = 0; i < noOfCombinations; i++) {
-            if (!contains(i, s)) {
-                s = s + i + ",";
-                doForRemaining(s, noOfCombinations, collector, rotationalIndex);
-            }
-        }
+        int num = nums[i];
 
+        //print the others
+        printOthers(nums, i);
+        printOthersReverse(nums, i);
 
-        String s1 = s;
-        String[] split = s1.split(",");
-        //if (split.length == noOfCombinations) {
-        //shift array
-        //for (int i = noOfCombinations - 1; i > 0; i--) {
-        if (rotationalIndex > 0) {
-
-            for (int i = rotationalIndex; i < noOfCombinations; i++) {
-                String temp = split[i - 1];
-                split[i - 1] = split[i];
-                split[i] = temp;
-            }
-            String s2 = toString(split, rotationalIndex);
-            //System.out.println(s2);
-            doForRemaining(s2, noOfCombinations, collector, rotationalIndex - 1);
-        }
-
-        //}
-
-        //take out last element
-        /*String s1 = s;
-        String[] split = s1.split(",");
-        if (split.length == noOfCombinations) {
-
-            if(split[0].equals(""+(noOfCombinations-1))){
-                return;
-            }
-            s = "";
-            //TODO :- need to fix this logic.
-            for (int i = 0; i < split.length - 2; i++) {
-
-                    s = s + split[i] + ",";
-
-            }
-            s = s + split[split.length - 1] + ",";
-            doForRemaining(s,noOfCombinations, collector);
-        }*/
+        traverse(nums, i + 1);
     }
 
-    private String toString(String[] split, int rotationalIndex) {
-        String s = "";
-        for (int i = 0; i < rotationalIndex; i++) {
-            s = s + split[i] + ",";
-        }
-        return s;
-    }
+    private void printOthersReverse(int[] nums, int location) {
+        System.out.println();
 
-    private boolean contains(int i, String s) {
-        StringTokenizer tokenizer = new StringTokenizer(s, ",");
-        while (tokenizer.hasMoreElements()) {
-            String s1 = tokenizer.nextToken();
-            if (s1.trim().equals("" + i)) {
-                return true;
+        for (int i = 0; i < nums.length; i++) {
+            if (i != location) {
+                System.out.print(nums[i]);
             }
         }
-
-        return false;
+        System.out.print(nums[location]);
     }
 
-    /*private void printCombinations(int noOfCombinations) {
-        for (int i = 0; i < noOfCombinations; i++) {
-            //get ith element and insert in between except for i
-            //insert xth at position
-            //ith ele
-            insert(i, noOfCombinations);
+    private void printOthers(int[] num, int location) {
+        System.out.println();
+        System.out.print(num[location]);
 
+        for (int i = 0; i < num.length; i++) {
+            if (i != location) {
+                System.out.print(num[i]);
+            }
         }
     }
-
-    private void insert(int i, int noOfCombinations) {
-        for (int positions = 0; positions < noOfCombinations; positions++) {
-            //insert at position
-            printAtPosition(i, positions, noOfCombinations);
-            System.out.println();
-        }
-    }
-
-    private void printAtPosition(int i, int positions, int noOfCombinations) {
-        int otherElements = i == 0 ? i + 1 : 0;
-        for (int j = 0; j < noOfCombinations; j++) {
-
-            if (otherElements == i) {
-                otherElements = i + 1;
-            }
-
-            if (otherElements >= noOfCombinations) {
-                otherElements = 0;
-            }
-
-
-            if (j == positions) {
-                System.out.print(" " + i);
-            } else {
-                System.out.print(" " + otherElements);
-                otherElements++;
-            }
-        }
-    }*/
-
 }
--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/Permutations.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/Permutations.java
@@ -23,14 +23,54 @@ public class Permutations {
 
     public static void main(String[] args) {
         Permutations p = new Permutations();
-        p.testCase(new int[]{1,2,3});
+        p.testCase(new int[]{1, 2, 3});
     }
 
     private void testCase(int[] ints) {
-
+        permute(ints);
     }
 
     public List<List<Integer>> permute(int[] nums) {
+        traverse(nums, 0);
         return null;
     }
+
+    private void traverse(int[] nums, int i) {
+
+        if (i >= nums.length) {
+            System.out.println();
+            return;
+        }
+
+        int num = nums[i];
+
+        //print the others
+        printOthers(nums, i);
+        printOthersReverse(nums, i);
+
+        traverse(nums, i + 1);
+    }
+
+    private void printOthersReverse(int[] nums, int location) {
+        System.out.println();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != location) {
+                System.out.print(nums[i]);
+            }
+        }
+        System.out.print(nums[location]);
+    }
+
+    private void printOthers(int[] num, int location) {
+        System.out.println();
+        System.out.print(num[location]);
+
+        for (int i = 0; i < num.length; i++) {
+            if (i != location) {
+                System.out.print(num[i]);
+            }
+        }
+    }
+
 }
