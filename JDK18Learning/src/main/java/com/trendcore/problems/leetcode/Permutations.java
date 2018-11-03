9d6ed69301de4a071d66d47a197b8ca0c63618ee
anurag <anurag.botre.202@gmail.com>
2018-11-03 11:50:57 +0530

    https://leetcode.com/problems/permutations/ 46. permutations
    - Implemented Algo
    - Need to provide finishing touch.

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/Permutations.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/Permutations.java
@@ -1,6 +1,9 @@
 package com.trendcore.problems.leetcode;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/permutations/
@@ -23,7 +26,7 @@ public class Permutations {
 
     public static void main(String[] args) {
         Permutations p = new Permutations();
-        p.testCase(new int[]{1, 2, 3});
+        p.testCase(new int[]{1, 2, 3, 4, 5});
     }
 
     private void testCase(int[] ints) {
@@ -32,25 +35,58 @@ public class Permutations {
 
     public List<List<Integer>> permute(int[] nums) {
         //TODO This solution is incomplete.
-        //may need to rotate array and then have to re-traverse.
-        traverse(nums, 0);
+        //create data structure to store unique combination
+
+        Set set = new HashSet() {
+            @Override
+            public Iterator iterator() {
+                return super.iterator();
+            }
+        };
+
+        traverse(nums, 0, set);
         return null;
     }
 
-    private void traverse(int[] nums, int i) {
+    private void traverse(int[] nums, int i, Set set) {
 
         if (i >= nums.length) {
-            System.out.println();
+            //System.out.println();
             return;
         }
 
         int num = nums[i];
 
         //print the others
-        printOthers(nums, i);
-        printOthersReverse(nums, i);
+        /*printOthers(nums, i);
+        printOthersReverse(nums, i);*/
+        printCombinations(nums, i, set);
 
-        traverse(nums, i + 1);
+        traverse(nums, i + 1, set);
+    }
+
+    private void printCombinations(int[] nums, int position, Set set) {
+        for (int i = 0; i < nums.length; i++) {
+            //int temp[] = new int[nums.length];
+            String str = "";
+            for (int j = position + 1, cnt = 0; cnt < nums.length; cnt++) {
+                if (j >= nums.length) {
+                    j = 0;
+                }
+                if (i == cnt) {
+                    System.out.print(" " + nums[position]);
+                    str = str + nums[position] + ",";
+                } else {
+                    System.out.print(" " + nums[j]);
+                    str = str + nums[j] + ",";
+                    j++;
+                }
+            }
+
+            set.add(str);
+            //System.out.println();
+        }
+        System.out.println();
     }
 
     private void printOthersReverse(int[] nums, int location) {
