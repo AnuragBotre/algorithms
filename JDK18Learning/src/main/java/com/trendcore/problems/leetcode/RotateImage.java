6ab2fe4436664be961ec928cb3b499d8c486a659
anurag <anurag.botre.202@gmail.com>
2018-11-05 22:33:17 +0530

    https://leetcode.com/problems/rotate-image/
    You are given an n x n 2D matrix representing an image.
    Rotate the image by 90 degrees (clockwise).
    
    - Implemented algo is not working
    - Need to implement another idea

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/RotateImage.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/RotateImage.java
@@ -54,13 +54,18 @@ public class RotateImage {
      */
     public static void main(String[] args) {
         RotateImage r = new RotateImage();
-        r.testCase(new int[][]{
+        /*r.testCase(new int[][]{
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9},
+        });*/
+
+        r.testCase(new int[][]{
+                {5, 1, 9,11},
+                {2, 4, 8, 10},
+                {13, 3, 6, 7},
+                {15, 14, 12, 16}
         });
-
-
     }
 
     private void testCase(int[][] matrix) {
@@ -72,7 +77,7 @@ public class RotateImage {
     private void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j]);
+                System.out.print(" " + matrix[i][j]);
             }
             System.out.println();
         }
@@ -80,31 +85,41 @@ public class RotateImage {
 
     public void rotate(int[][] matrix) {
 
+
+        //Idea 2 rotation
+        //visit each and every position
+        //traverse diagonally
+
+
         for (int i = 0, row = 0; i < matrix.length; i++, row++) {
             int oThEleOfRow = matrix[row][0];
-            for (int j = matrix.length - 1 , col = 0; j >= 0; j-- , col++) {
+            for (int j = matrix.length - 1, col = 0; j >= 0; j--, col++) {
 
-                if(col == matrix.length - 1){
+                /*if (row == matrix.length - 1 && col > 0)
+                    break;*/
+
+                if (col == matrix.length - 1) {
                     int temp = matrix[row][col];
                     matrix[row][col] = matrix[matrix.length - 1][row];
                     matrix[matrix.length - 1][row] = temp;
-                }else{
+                } else {
                     int temp = matrix[row][col];
                     matrix[row][col] = matrix[j][i];
                     matrix[j][i] = temp;
                 }
 
                 //System.out.print(matrix[j][i]);
-                debug(matrix);
+                debug(matrix, row, col);
             }
             System.out.println();
         }
     }
 
-    private void debug(int[][] matrix) {
+    private void debug(int[][] matrix, int row, int col) {
         Scanner s = new Scanner(System.in);
         String next = s.next();
         System.out.println();
+        System.out.println(" ROw :- " + row + " Col :- " + col);
         printMatrix(matrix);
     }
 
