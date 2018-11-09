0a1a08c7d8bc6f32b1e4859640dfe967673d9616
anurag <anurag.botre.202@gmail.com>
2018-11-09 09:13:53 +0530

    Java Query DSL - Can be used for kick start.

---
+++ javaq/src/main/java/com/trendcore/Main.java
@@ -0,0 +1,12 @@
+package com.trendcore;
+
+import static com.javaq.JavaQ.select;
+
+public class Main {
+
+    public static void main(String[] args) {
+        select().from("EMPLOYEE").sql();
+
+    }
+
+}
