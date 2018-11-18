3f29bdb30807c7ecd32148308940ec4f45377dfe
anurag <anurag.botre.202@gmail.com>
2018-11-18 22:12:15 +0530

    SQL DSL

--- query-dsl-study/src/main/java/com/trendcore/Main.java
+++ query-dsl-study/src/main/java/com/trendcore/Main.java
@@ -1,6 +1,7 @@
 package com.trendcore;
 
 import com.querydsl.core.support.QueryBase;
+import com.querydsl.jpa.OpenJPATemplates;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.trendcore.entity.QPerson;
 
@@ -11,9 +12,14 @@ public class Main {
     public static void main(String[] args) {
         QPerson person = QPerson.person;
 
-        JPAQuery query = new JPAQuery(null);
-        QueryBase kent = query.from(person).where(person.firstname.eq("Kent"));
+        JPAQuery query = new JPAQuery();
+        QueryBase kent = query.from(person).where(
+                person.firstname.eq("Kent")
+                        .and(person.surname.eq("beck")),
+                person.surname.eq("test")
+            );
 
+        System.out.println(query.toString());
 
     }
 }
