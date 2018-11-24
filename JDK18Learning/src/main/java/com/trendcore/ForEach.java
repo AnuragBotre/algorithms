18205458d938675d17c856b4daf271e5c1d64fbb
anurag <anurag.botre.202@gmail.com>
2018-11-24 11:55:11 +0530

    Notes

--- JDK18Learning/src/main/java/com/trendcore/ForEach.java
+++ JDK18Learning/src/main/java/com/trendcore/ForEach.java
@@ -18,9 +18,31 @@ public class ForEach {
                 .collect(Collectors.toList());
 
 
+        ForEach forEach = new ForEach();
+        list.forEach(integer -> forEach.print(integer));
+
+        list.forEach(integer -> staticPrint(integer));
+
         System.out.println(newList.hashCode());
 
         System.out.println(list.hashCode());
+
+        forEach.test();
+
+
+    }
+
+    private void test() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        list.forEach(integer -> print(integer));
+    }
+
+    public void print(Integer integer){
+
+    }
+
+    public static void staticPrint(Integer integer){
+
     }
 
 }
