278e8ad6681a0875606578946e4f477165736f63
anurag <anurag.botre.202@gmail.com>
2018-12-07 02:13:15 +0530

    Refactoring - Refactored Select Stream.

--- transaction-management/src/main/java/com/trendcore/SelectStream.java
+++ transaction-management/src/main/java/com/trendcore/SelectStream.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import java.sql.*;
 import java.util.Spliterators;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -18,10 +19,12 @@ public class SelectStream {
         this.dataSource = dataSource;
     }
 
+
     public <T extends Table> Stream<Row<T>> stream(Class<T> t, String sql, Object... params) {
 
         ProxyConnectionForStream proxyConnectionForStream = new ProxyConnectionForStream();
 
+        @SuppressWarnings(value = {"unchecked"})
         ResultSetIterator<Row<T>> resultSetIterator = new ResultSetIterator<>()
                                         .resultSet(() -> {
                                             try {
@@ -42,9 +45,7 @@ public class SelectStream {
         Stream<Row<T>> stream = StreamSupport
                 .stream(Spliterators.spliteratorUnknownSize(resultSetIterator, 0), false);
 
-        stream.onClose(() -> {
-            proxyConnectionForStream.close();
-        });
+        stream.onClose(proxyConnectionForStream::close);
 
         return stream;
     }
@@ -52,9 +53,11 @@ public class SelectStream {
     public Stream<Row> stream(String sql, Object... params) {
         ProxyConnectionForStream proxyConnectionForStream = new ProxyConnectionForStream();
 
-        BiFunction<ResultSetMetaData, ResultSet, Row> mapper = (metaData, resultSet) -> Table.row(metaData,resultSet);
+        BiFunction<ResultSetMetaData, ResultSet, Row> mapper = Table::row;
 
+        Consumer<SingleResultSetWrapper> singleResultSetWrapperConsumer = SingleResultSetWrapper::close;
 
+        @SuppressWarnings(value = {"unchecked"})
         ResultSetIterator<Row> resultSetIterator = new ResultSetIterator<Row>()
                 .resultSet(() -> {
                     try {
@@ -68,16 +71,12 @@ public class SelectStream {
 
                 })
                 .resultSetMapper(mapper)
-                .onClose(singleResultSetWrapper -> {
-                    proxyConnectionForStream.close();
-                });
+                .onClose(singleResultSetWrapperConsumer);
 
         Stream<Row> stream = StreamSupport
                 .stream(Spliterators.spliteratorUnknownSize(resultSetIterator, 0), false);
 
-        stream.onClose(() -> {
-            proxyConnectionForStream.close();
-        });
+        stream.onClose(proxyConnectionForStream::close);
 
         return stream;
     }
