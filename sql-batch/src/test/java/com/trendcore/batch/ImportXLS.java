ec250d2f246010104502a5bce0cac251acdc2a6d
anurag <anurag.botre.202@gmail.com>
2018-12-09 12:55:45 +0530

    Import Test -Reading XLS file.

--- sql-batch/src/test/java/com/trendcore/batch/ImportXLS.java
+++ sql-batch/src/test/java/com/trendcore/batch/ImportXLS.java
@@ -7,10 +7,11 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class ImportXLS {
 
@@ -23,7 +24,7 @@ public class ImportXLS {
                 .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
                 .open(is);
 
-        for (Sheet sheet : workbook){
+        for (Sheet sheet : workbook) {
             System.out.println(sheet.getSheetName());
             for (Row r : sheet) {
                 for (Cell c : r) {
@@ -31,5 +32,25 @@ public class ImportXLS {
                 }
             }
         }
+    }
+
+    @Test
+    public void creatingInputStream() {
+        InputStream is = ImportXLS.class.getClassLoader().getResourceAsStream("actor-new.xlsx");
+        Workbook workbook = StreamingReader.builder()
+                .rowCacheSize(100)    // number of rows to keep in memory (defaults to 10)
+                .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
+                .open(is);
+
+        workbook.sheetIterator().forEachRemaining(sheets -> processSheets(sheets));
+    }
+
+    private void processSheets(Sheet sheet) {
+        Stream<Row> stream = StreamSupport.stream(sheet.spliterator(), false);
+        stream.skip(1).forEach(cells -> {
+            for(Cell c : cells){
+                System.out.println(c.getStringCellValue());
+            }
+        });
     }
 }
