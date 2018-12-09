1ea2dbca58d5125b9c1d8ff8b3515c0ee0561049
anurag <anurag.botre.202@gmail.com>
2018-12-09 12:11:54 +0530

    Import Test -Reading XLS file.

--- sql-batch/src/test/java/com/trendcore/batch/ImportXLS.java
+++ sql-batch/src/test/java/com/trendcore/batch/ImportXLS.java
@@ -1,12 +1,35 @@
 package com.trendcore.batch;
 
+import com.monitorjbl.xlsx.StreamingReader;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class ImportXLS {
 
-
     @Test
-    public void readXLSAndPrintOnConsole() {
+    public void readXLSAndPrintOnConsole() throws FileNotFoundException {
 
+        InputStream is = ImportXLS.class.getClassLoader().getResourceAsStream("actor-new.xlsx");
+        Workbook workbook = StreamingReader.builder()
+                .rowCacheSize(100)    // number of rows to keep in memory (defaults to 10)
+                .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
+                .open(is);
+
+        for (Sheet sheet : workbook){
+            System.out.println(sheet.getSheetName());
+            for (Row r : sheet) {
+                for (Cell c : r) {
+                    System.out.println(c.getStringCellValue());
+                }
+            }
+        }
     }
 }
