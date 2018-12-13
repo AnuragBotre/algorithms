f0e3c6d61e2a34a9ffc6da51e709014e120e1050
anurag <anurag.botre.202@gmail.com>
2018-12-13 22:14:23 +0530

    Import Test.

---
+++ transaction-management/src/main/java/com/trendcore/SQLDMLCommand.java
@@ -0,0 +1,15 @@
+package com.trendcore;
+
+import java.sql.Connection;
+
+public class SQLDMLCommand {
+
+
+    public SQLDMLCommand(Connection connection){
+    }
+
+    //prepare phase where prepare statement will be formed
+
+    //run phase
+
+}
