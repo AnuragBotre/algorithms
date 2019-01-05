d8bac14cb06cf0fc78a15b422e46d94a7b8435cd
anurag <anurag.botre.202@gmail.com>
2019-01-06 00:59:16 +0530

    1st Integration

--- http-rpc-study/src/main/java/com/com/trendcore/rest/NewSevlet.java
+++ http-rpc-study/src/main/java/com/com/trendcore/rest/NewSevlet.java
@@ -1,17 +1,40 @@
 package com.com.trendcore.rest;
 
+import com.trendcore.HikariDataSource;
+import com.trendcore.SelectStream;
+import com.trendcore.sql.Column;
+import com.trendcore.sql.Row;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Stream;
 
 @WebServlet(urlPatterns={"/newServlet/*"})
 public class NewSevlet extends HttpServlet {
 
+    private final DataSource dataSource;
+
+    public NewSevlet() {
+        dataSource = HikariDataSource.get().getDataSource();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Response from new Servlet.");
+        SelectStream select = new SelectStream(dataSource);
+        Stream<Row> stream = select.stream("select * from address a where a.address = ?", 1);
+
+        PrintWriter writer = resp.getWriter();
+
+        stream.forEach(actorRow -> {
+            Column id = new Column();
+            id.setIndex(1);
+            writer.println(actorRow.get(id));
+        });
     }
 }
