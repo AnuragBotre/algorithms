25e07f542e04ebac1ea4022282420b3ab585a91a
anurag <anurag.botre.202@gmail.com>
2019-02-17 23:41:00 +0530

    Jump Game
    - Algo Implemented.
    - Leet code solution
      most clean way is implemented by them

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/JumpGame.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/JumpGame.java
@@ -28,24 +28,49 @@ public class JumpGame {
 
     public static void main(String[] args) {
         JumpGame j = new JumpGame();
-        j.testCase(new int[]{0});
-        j.testCase(new int[]{1, 2});
-        j.testCase(new int[]{2, 3, 1, 1, 4});
-        j.testCase(new int[]{3, 2, 1, 0, 4});
-        j.testCase(new int[]{3, 4, 1, 1, 1, 1, 0, 4});
+        j.testCase(new int[]{0}, true);
+        j.testCase(new int[]{1, 2}, true);
+        j.testCase(new int[]{2, 0}, true);
+        j.testCase(new int[]{0, 2, 3}, false);
+        j.testCase(new int[]{3, 0, 8, 2, 0, 0, 1}, true);
+        j.testCase(new int[]{2, 3, 1, 0, 0, 0, 1, 4}, false);
+        j.testCase(new int[]{2, 3, 1, 1, 4}, true);
+        j.testCase(new int[]{3, 2, 1, 0, 4}, false);
+        j.testCase(new int[]{3, 4, 1, 1, 1, 1, 0, 4}, false);
+        j.testCase(new int[]{8, 2, 4, 4, 4, 9, 5, 2, 5, 8, 8, 0, 8, 6, 9, 1, 1, 6, 3, 5, 1, 2, 6, 6, 0, 4, 8, 6, 0, 3, 2, 8, 7, 6, 5, 1, 7, 0, 3, 4, 8, 3, 5, 9, 0, 4, 0, 1, 0, 5, 9, 2, 0, 7, 0, 2, 1, 0, 8, 2, 5, 1, 2, 3, 9, 7, 4, 7, 0, 0, 1, 8, 5, 6, 7, 5, 1, 9, 9, 3, 5, 0, 7, 5}, true);
     }
 
-    private void testCase(int[] nums) {
-        System.out.println(canJump(nums));
+    private void testCase(int[] nums, boolean expected) {
+        System.out.println(canJump(nums) + " " + expected);
     }
 
     public boolean canJump(int[] nums) {
 
-        List<Integer> list = new ArrayList();
+        int[] dp = new int[nums.length + 1];
 
-        /*for (int i = 0; i < intermediateStages.length; i++) {
-            intermediateStages[i] = Integer.MAX_VALUE;
-        }*/
+        for (int i = nums.length - 2; i >= 0; --i) {
+            int k = nums[i];
+            if (k + i >= nums.length - 1) { /* At last element */
+                dp[i] = 1;
+                continue;
+            }
+
+            dp[i] = Integer.MAX_VALUE;
+
+            for (; k > 0; --k) {
+                if (dp[i + k] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i + k] + 1, dp[i]);
+            }
+        }
+        return dp[0] != Integer.MAX_VALUE;
+    }
+
+    public boolean approach1(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+
+        List<Integer> list = new ArrayList();
         int currentStep = 0;
         for (; currentStep < nums.length - 1; ) {
             //can we jump from the current step and how many jump allowed
@@ -54,6 +79,9 @@ public class JumpGame {
             if (allowedJumps + (currentStep + 1) >= nums.length) {
                 return true;
             } else if (allowedJumps == 0) {
+                if (list.isEmpty()) {
+                    return false;
+                }
                 currentStep = list.remove(0);
             }
 
@@ -91,6 +119,29 @@ public class JumpGame {
         }
 
         return false;
+    }
+
+
+    /*
+    Greedy solution. We maintain the variable "reach" to tell how far we can reach based on the total nums we've accumulated so far.
+    At each iteration of nums[i], we add this to the "reach" and
+    compare this value with the current reach. Take the max of the two as the new reach.
+    The loop should end when i is reaching "reach" meaning we can't go futher.
+    Or when i is reaching n where n = nums.length
+    At the end we check if i == n then we know it reaches the end.
+    */
+    class Solution {
+        public boolean canJump(int[] nums) {
+            int n = nums.length;
+            int i = 0;
+            int reach = 0;
+            while (i < n && i <= reach) {
+                reach = Math.max(i + nums[i], reach);
+                i++;
+            }
+
+            return i == n;
+        }
     }
 
 }
