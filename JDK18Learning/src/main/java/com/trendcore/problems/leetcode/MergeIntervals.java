0f4d49ccc2d82c25ccb4ebc3f2677732280b0290
anurag <anurag.botre.202@gmail.com>
2019-02-19 19:53:59 +0530

    56. Merge Intervals
    https://leetcode.com/problems/merge-intervals/
    
    - Partial Implementation.

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/MergeIntervals.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/MergeIntervals.java
@@ -1,9 +1,6 @@
 package com.trendcore.problems.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -41,6 +38,24 @@ public class MergeIntervals {
             start = s;
             end = e;
         }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) return true;
+            if (object == null || getClass() != object.getClass()) return false;
+
+            Interval interval = (Interval) object;
+
+            if (start != interval.start) return false;
+            return end == interval.end;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = start;
+            result = 31 * result + end;
+            return result;
+        }
     }
 
 
@@ -48,11 +63,36 @@ public class MergeIntervals {
         MergeIntervals mergeIntervals = new MergeIntervals();
 
         mergeIntervals.testCase(new int[][]{
+                {1, 3}
+        });
+
+        mergeIntervals.testCase(new int[][]{
                 {1, 3}, {2, 6}, {8, 10}, {15, 18}
         });
 
+        mergeIntervals.testCase(new int[][]{
+                {1, 4}, {4, 5}
+        });
 
-        mergeIntervals.merge(null);
+        mergeIntervals.testCase(new int[][]{
+                {1, 4}, {0, 4}
+        });
+
+        mergeIntervals.testCase(new int[][]{
+                {1, 4}, {0, 1}
+        });
+
+        mergeIntervals.testCase(new int[][]{
+                {1, 4}, {0, 0}
+        });
+
+        mergeIntervals.testCase(new int[][]{
+                {1, 4}, {0, 2}, {3, 5}
+        });
+
+        mergeIntervals.testCase(new int[][]{
+                {2, 3}, {4, 5}, {6, 7}, {8, 9}, {1, 10}
+        });
     }
 
     private void testCase(int[][] ints) {
@@ -60,19 +100,70 @@ public class MergeIntervals {
         List<Interval> collect = Stream.of(ints).map(ints1 -> new Interval(ints1[0], ints1[1])).collect(Collectors.toList());
 
         System.out.println();
-        merge(collect).forEach(interval -> System.out.println(interval.start + " " + interval.end));
+        merge(collect).forEach(interval -> {
+            System.out.print("[");
+            System.out.print(interval.start + " " + interval.end);
+            System.out.print("]");
+        });
     }
 
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> mergedIntervals = new ArrayList<>();
 
-        Map map = new HashMap();
+        if (intervals.size() == 1) {
+            mergedIntervals.add(intervals.get(0));
+            return mergedIntervals;
+        }
 
         for (int i = 0; i < intervals.size(); i++) {
-            //TODO : Need to check options
-            //
+
+            Interval interval = intervals.get(i);
+            if (i == 0) {
+                mergedIntervals.add(interval);
+            } else {
+
+                boolean found = false;
+                for (int j = 0; j < mergedIntervals.size(); j++) {
+                    Interval interInterval = mergedIntervals.get(j);
+                    if (intervalCanbeMerged(interval, interInterval)) {
+                        //create merged interInterval
+                        int start = interval.start < interInterval.start ? interval.start : interInterval.start;
+                        int end = interval.end > interInterval.end ? interval.end : interInterval.end;
+                        interInterval.start = start;
+                        interInterval.end = end;
+                        found = true;
+                    }
+                }
+
+                if(!found){
+                    mergedIntervals.add(interval);
+                }
+            }
+
+
         }
+
         return mergedIntervals;
+    }
+
+    //1,4     0,0
+    //1,3     2,4
+    //1,3     3,4
+    //1,4     0,0
+    //2,5     3,4
+    //2,5     5,6
+    private boolean intervalCanbeMerged(Interval interval, Interval interval1) {
+        if (interval1.start <= interval.start && interval1.end >= interval.start) {
+            return true;
+        } else if (interval1.start <= interval.end && interval1.end >= interval.end) {
+            return true;
+        } else if (interval.start <= interval1.start && interval.end >= interval1.start) {
+            return true;
+        } else if (interval.start <= interval1.end && interval.end >= interval1.end) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
