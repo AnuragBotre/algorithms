6a5b2d590b94c4d00eab0a7700f257689a805a58
anurag <anurag.botre.202@gmail.com>
2019-05-25 23:59:29 +0530

    OffHeap Map - Off Heap understanding

--- OffHeapCache/src/main/java/com/trendcore/offheap/cache/Main.java
+++ OffHeapCache/src/main/java/com/trendcore/offheap/cache/Main.java
@@ -9,12 +9,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        OffHeapMap o = new OffHeapMap(null, 16, 0);
+        OffHeapMap o = new OffHeapMap(null, 1, 0);
         OffHeapMapAdapter<Object> offHeapMap = new OffHeapMapAdapter(o);
 
         offHeapMap.put("abc","test");
         offHeapMap.put("pqe",new User(1));
         offHeapMap.put("xyz",new User(2));
+
+        Object abc = offHeapMap.get("abc");
+        System.out.println(((Value)abc).get());
 
         offHeapMap.entrySet().stream().forEach(o1 -> System.out.println(((Value)((Map.Entry)o1).getValue()).get()));
     }
