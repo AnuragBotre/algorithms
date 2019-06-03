703f73d1797d35c6b7aea652263e2b6aea42cb04
anurag <anurag.botre.202@gmail.com>
2019-06-03 18:59:54 +0530

    Collectors
    Sequential and parallels.
    
    Code Refactoring.

--- design-pattern/src/main/java/collector/Main.java
+++ design-pattern/src/main/java/collector/Main.java
@@ -13,29 +13,14 @@ public class Main {
                 asList("two:one", "two:two", "two:three"),
                 asList("three:one", "three:two", "three:three", "three:four"));
 
-        List list = new ArrayList();
-
-        List collect = nestedList.stream().flatMap(strings -> strings.stream()).collect(
-                () -> list,
-                (list12, s) -> list12.add(s),
-                (list1, list2) ->
-                //This method will be never used in sequential stream.
-                //A careful reading of the streams implementation code in ReduceOps.java
-                // reveals that the combine function is called only when a ReduceTask completes,
-                // and ReduceTask instances are used only when evaluating a pipeline in parallel.
-                // Thus, in the current implementation,
-                // the combiner is never called when evaluating a sequential pipeline.
-                {
-                    //list1.addAll(list2);
-                    System.out.println(list1 + " " + list2);
-                });
-
-        collect.forEach(o -> System.out.println(o));
-
-        nestedList.forEach(strings -> System.out.println(strings));
+        sequential(nestedList);
 
 
+        parallel(nestedList);
 
+    }
+
+    private static void parallel(List<List<String>> nestedList) {
         System.out.println("Doing it in parallel");
         /**
          * Above code () -> list, supplying global list will not work in case of parallel streams.
@@ -62,7 +47,32 @@ public class Main {
                 });
 
         collect1.forEach(o -> System.out.println(o));
+    }
 
+    private static void sequential(List<List<String>> nestedList) {
+        List list = new ArrayList();
+
+        List collect = nestedList.stream().flatMap(strings -> strings.stream()).collect(
+                //This is wrong never return global instance
+                //In case of sequential this will work but in case of parallel
+                //it will not work.
+                () -> list,
+                (list12, s) -> list12.add(s),
+                (list1, list2) ->
+                //This method will be never used in sequential stream.
+                //A careful reading of the streams implementation code in ReduceOps.java
+                // reveals that the combine function is called only when a ReduceTask completes,
+                // and ReduceTask instances are used only when evaluating a pipeline in parallel.
+                // Thus, in the current implementation,
+                // the combiner is never called when evaluating a sequential pipeline.
+                {
+                    //list1.addAll(list2);
+                    System.out.println(list1 + " " + list2);
+                });
+
+        collect.forEach(o -> System.out.println(o));
+
+        nestedList.forEach(strings -> System.out.println(strings));
     }
 
 }
