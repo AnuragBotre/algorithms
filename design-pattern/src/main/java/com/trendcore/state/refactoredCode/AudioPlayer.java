11a253153a2d9d5a5ef27a1f2ccd0c5017d5ec02
anurag <anurag.botre.202@gmail.com>
2019-06-17 18:52:44 +0530

    State Pattern Example - 4
    Player Example

---
+++ design-pattern/src/main/java/com/trendcore/state/refactoredCode/AudioPlayer.java
@@ -0,0 +1,11 @@
+package com.trendcore.state.refactoredCode;
+
+public class AudioPlayer {
+
+    Playable playable;
+
+    //Refer this
+    //https://refactoring.guru/design-patterns/state
+
+
+}
---
+++ design-pattern/src/main/java/com/trendcore/state/refactoredCode/Playable.java
@@ -0,0 +1,12 @@
+package com.trendcore.state.refactoredCode;
+
+public interface Playable {
+
+    void clickLock();
+
+    void clickPlay();
+
+    void clickNext();
+
+    void clickPrevious();
+}
