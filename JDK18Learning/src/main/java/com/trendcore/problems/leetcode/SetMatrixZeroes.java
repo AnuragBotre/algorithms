3257eccd8c80a04c1a44750587c619e4cd0f4631
anurag <anurag.botre.202@gmail.com>
2019-09-05 00:43:17 +0530

    73. Set Matrix Zeroes
    https://leetcode.com/problems/set-matrix-zeroes/
    
    Algo implemented accepted -> with constant space complexity.

--- JDK18Learning/src/main/java/com/trendcore/problems/leetcode/SetMatrixZeroes.java
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/SetMatrixZeroes.java
@@ -47,6 +47,76 @@ public class SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
+
+        int tempRow[] = new int[cols];
+
+        for (int i = 0; i < rows; i++) {
+
+            //i = 0
+            if (i == 0) {
+                //copy in tempRow
+                copyRow(matrix, i, tempRow);
+            }
+
+
+            //is 0 present and at which location
+
+            Object[] objects = rowContainsZero(matrix[i]);
+
+            boolean zeroPresentInRow = (boolean) objects[0];
+
+            if (zeroPresentInRow) {
+
+                //mark entire row 0
+                for (int x = 0; x < cols; x++) {
+
+                    if(matrix[i][x] == 0){
+                        tempRow[x] = 0;
+
+
+                        //go for prev row with 0
+                        for (int y = i; y >= 0; y--) {
+                            matrix[y][x] = 0;
+                        }
+                    }
+
+                    matrix[i][x] = 0;
+                }
+
+
+            } else {
+
+                //process temp row
+
+                for (int x = 0 ; x < cols ; x++) {
+                    if(tempRow[x] == 0){
+                        matrix[i][x] = 0;
+                    }
+                }
+            }
+
+        }
+    }
+
+    private Object[] rowContainsZero(int[] row) {
+
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] == 0) {
+                return new Object[]{true, i};
+            }
+        }
+        return new Object[]{false, 0};
+    }
+
+    private void copyRow(int[][] matrix, int i, int[] tempRow) {
+        for (int x = 0; x < tempRow.length; x++) {
+            tempRow[x] = matrix[i][x];
+        }
+    }
+
+    public void bruteForceHighSpaceComplexity(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         int state[][] = new int[rows][cols];
 
         for (int i = 0; i < rows; i++) {
--- JDK18Learning/src/test/java/com/trendcore/problems/leetcode/SetMatrixZeroesTest.java
+++ JDK18Learning/src/test/java/com/trendcore/problems/leetcode/SetMatrixZeroesTest.java
@@ -36,6 +36,31 @@ public class SetMatrixZeroesTest {
         });
     }
 
+    @Test
+    public void for_faileInput1() {
+
+
+        act(new int[][]{
+                {3, 5, 5, 6, 9, 1, 4, 5, 0, 5},
+                {2, 7, 9, 5, 9, 5, 4, 9, 6, 8},
+                {6, 0, 7, 8, 1, 0, 1, 6, 8, 1},
+                {7, 2, 6, 5, 8, 5, 6, 5, 0, 6},
+                {2, 3, 3, 1, 0, 4, 6, 5, 3, 5},
+                {5, 9, 7, 3, 8, 8, 5, 1, 4, 3},
+                {2, 4, 7, 9, 9, 8, 4, 7, 3, 7},
+                {3, 5, 2, 8, 8, 2, 2, 4, 9, 8}
+        }, new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {2, 0, 9, 5, 0, 0, 4, 9, 0, 8},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {5, 0, 7, 3, 0, 0, 5, 1, 0, 3},
+                {2, 0, 7, 9, 0, 0, 4, 7, 0, 7},
+                {3, 0, 2, 8, 0, 0, 2, 4, 0, 8}
+        });
+    }
+
     public void act(int[][] matrix, int[][] expected) {
         setMatrixZeroes.setZeroes(matrix);
 
@@ -45,15 +70,23 @@ public class SetMatrixZeroesTest {
         int cols = matrix[0].length;
 
         for (int i = 0; i < rows; i++) {
+            StringBuilder errorLocation = new StringBuilder();
             for (int j = 0; j < cols; j++) {
                 System.out.print(matrix[i][j] + " ");
                 try {
                     assertEquals(expected[i][j], matrix[i][j]);
                 } catch (Throwable e) {
                     notValid = true;
+                    errorLocation.append(" i,j -> " + i + "," + j);
+                    errorLocation.append("\r\n");
                 }
             }
+
             System.out.println();
+            String errorLocationString = errorLocation.toString();
+            if(!errorLocationString.isEmpty())
+                System.out.println(errorLocationString);
+
         }
 
         assertTrue(!notValid);
