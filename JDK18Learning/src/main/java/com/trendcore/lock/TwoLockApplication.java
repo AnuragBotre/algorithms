8b69cff4ac708260ca068311cf11afedaee5162e
anurag <anurag.botre.202@gmail.com>
2020-01-15 12:16:51 +0530

    Loggable Read Write Lock and its implementation.

---
+++ JDK18Learning/src/main/java/com/trendcore/lock/TwoLockApplication.java
@@ -0,0 +1,86 @@
+package com.trendcore.lock;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.LoggableReadWriteLock;
+
+public class TwoLockApplication {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        /*final ReadWriteLock readWriteLock1 = new ReentrantReadWriteLock();
+        final ReadWriteLock readWriteLock2 = new ReentrantReadWriteLock();*/
+
+        final BlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
+
+
+        final LoggableReadWriteLock readWriteLock1 = new LoggableReadWriteLock("Lock for Task1",linkedBlockingQueue);
+        final LoggableReadWriteLock readWriteLock2 = new LoggableReadWriteLock("Lock for Task2",linkedBlockingQueue);
+
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
+
+
+
+
+        Runnable task1 = new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while(i < 1000){
+                    try{
+                        readWriteLock1.writeLock().lock();
+                        try{
+                            readWriteLock2.writeLock().lock();
+                            System.out.println("Performing Task1.");
+                            //linkedBlockingQueue.add(new LoggableReadWriteReentrantLock.LogMessage(LoggableReadWriteReentrantLock.LockState.ACQUIRED, LoggableReadWriteReentrantLock.LockType.WRITE,Thread.currentThread(),"\tPerforming Task1",System.currentTimeMillis()));
+                        }finally {
+                            readWriteLock2.writeLock().unlock();
+                        }
+                    }finally {
+                        readWriteLock1.writeLock().unlock();
+                    }
+                    i++;
+                }
+                countDownLatch.countDown();
+            }
+        };
+
+        Runnable task2 = new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while(i < 1000) {
+                    try {
+                        readWriteLock2.writeLock().lock();
+                        System.out.println("Performing Task2.");
+                        //linkedBlockingQueue.add(new LoggableReadWriteReentrantLock.LogMessage(LoggableReadWriteReentrantLock.LockState.ACQUIRED, LoggableReadWriteReentrantLock.LockType.WRITE,Thread.currentThread(),"\tPerforming Task2",System.currentTimeMillis()));
+                    } finally {
+                        readWriteLock2.writeLock().unlock();
+                    }
+                    i++;
+                }
+                countDownLatch.countDown();
+            }
+        };
+
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+
+        t1.start();
+        t2.start();
+
+
+        countDownLatch.await();
+
+        /*readWriteLock1.showLog();
+        readWriteLock2.showLog();*/
+        /*for (LoggableReadWriteReentrantLock.LogMessage logMessage : linkedBlockingQueue) {
+            System.out.println(logMessage.label+","+logMessage.currentThread.getId()+","+logMessage.lockType.name()+","+logMessage.lockState.name()+","+logMessage.currentTimeMillis);
+        }*/
+
+    }
+
+
+}
