5f1012703611252386aedf4d470619dd22c83ad8
anurag <anurag.botre.202@gmail.com>
2020-01-29 00:20:11 +0530

    https://leetcode.com/problems/minimum-window-substring/
    76. Minimum Window Substring
    
    First Solution implemented.
    
    Not working for same input like aa

--- JDK18Learning/src/test/java/com/trendcore/problems/leetcode/MinimumWindowSubStringTest.java
+++ JDK18Learning/src/test/java/com/trendcore/problems/leetcode/MinimumWindowSubStringTest.java
@@ -15,6 +15,34 @@ public class MinimumWindowSubStringTest {
         act(s, t, output);
     }
 
+    @Test
+    public void intput2() {
+        String s = "ADOBECODEBANC", t = "AD";
+        String output = "AD";
+        act(s, t, output);
+    }
+
+    @Test
+    public void intput3() {
+        String s = "ADOBECODEBANC", t = "XY";
+        String output = "";
+        act(s, t, output);
+    }
+
+    @Test
+    public void input4() {
+        String s = "ADOBECODEBANC", t = "DBC";
+        String output = "DOBEC";
+        act(s, t, output);
+    }
+
+    @Test
+    public void failedInput_1() {
+        String s = "aa", t = "aa";
+        String output = "aa";
+        act(s, t, output);
+    }
+
     private void act(String s, String t, String output) {
         String s1 = m.minWindow(s, t);
         assertEquals(output,s1);
