9f5ec1a30ea928230a6025d3e3451d36376faddc
anurag <anurag.botre.202@gmail.com>
2020-02-10 18:45:25 +0530

    https://leetcode.com/problems/maximal-rectangle/
    85. Maximal Rectangle
    
    Initial Commit.

---
+++ JDK18Learning/src/main/java/com/trendcore/problems/leetcode/MaximalRectangle.java
@@ -0,0 +1,28 @@
+package com.trendcore.problems.leetcode;
+
+/**
+ * https://leetcode.com/problems/maximal-rectangle/
+ * <p>
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+ * <p>
+ * Example:
+ * Input:
+ * <p>
+ * [
+ * ["1","0","1","0","0"],
+ * ["1","0","1","1","1"],
+ * ["1","1","1","1","1"],
+ * ["1","0","0","1","0"]
+ * ]
+ * Output: 6
+ */
+public class MaximalRectangle {
+
+    public int maximalRectangle(char[][] matrix) {
+
+
+
+        return 6;
+    }
+
+}
---
+++ JDK18Learning/src/test/java/com/trendcore/problems/leetcode/MaximalRectangleTest.java
@@ -0,0 +1,25 @@
+package com.trendcore.problems.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaximalRectangleTest {
+
+    MaximalRectangle m = new MaximalRectangle();
+
+    @Test
+    public void input1() {
+        act(new char[][]{
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}
+        }, 6);
+    }
+
+    private void act(char[][] strings, int actual) {
+        int output = m.maximalRectangle(strings);
+        assertEquals(output, actual);
+    }
+}
