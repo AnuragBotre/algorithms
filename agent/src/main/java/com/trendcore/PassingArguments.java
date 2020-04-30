ccd15db61fd531250562b4864dca25884cad95dd
anurag <anurag.botre.202@gmail.com>
2020-05-01 00:23:36 +0530

    Java Profiling changing class signatures. - Passing class name to signature.

--- agent/src/main/java/com/trendcore/PassingArguments.java
+++ agent/src/main/java/com/trendcore/PassingArguments.java
@@ -1,12 +1,16 @@
 package com.trendcore;
 
 import java.lang.annotation.Annotation;
+import java.util.Date;
 
 public class PassingArguments {
 
     @Profile
-    public void method1(String a,String b){
-        Profiler.pushMethod("PassingArguments","profileMe",System.currentTimeMillis(),"java.lang.String,java.lang.String","HTTP_REQUEST",a,b);
+    public void method1(String a, String b, String c, int d, long e, float f, Date date){
+        Profiler.pushMethod("PassingArguments","profileMe",System.currentTimeMillis(),"java.lang.String,java.lang.String","HTTP_REQUEST",a,b,c,d,e,f,date);
+
+
+
     }
 
 }
