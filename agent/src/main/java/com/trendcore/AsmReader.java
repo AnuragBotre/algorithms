0969f42ca86c6cc1053c9473c36f062ddc58e834
anurag <anurag.botre.202@gmail.com>
2020-05-10 00:21:53 +0530

    Java Profiling changing class signatures. - Creating Jar In correct way.

--- agent/src/main/java/com/trendcore/AsmReader.java
+++ agent/src/main/java/com/trendcore/AsmReader.java
@@ -11,6 +11,12 @@ import java.util.Arrays;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+import java.util.zip.CRC32;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static java.util.jar.JarFile.MANIFEST_NAME;
 
 /**
  * http://modularity.info/conference/2007/program/industry/I5-UsingASMFramework.pdf
@@ -18,6 +24,8 @@ import java.util.jar.JarOutputStream;
  * Look for try / finally
  * <p>
  * http://web.cs.ucla.edu/~msb/cs239-tutorial/
+ *
+ * https://github.com/openjdk-mirror/jdk7u-jdk/blob/master/src/share/classes/sun/tools/jar/Main.java
  */
 
 public class AsmReader {
@@ -36,7 +44,22 @@ public class AsmReader {
         /*String dir = "E:\\profilation";
         scanDirectory(dir);*/
 
-        JarOutputStream dest = new JarOutputStream(new FileOutputStream(outpath+"data-structures-1.0-SNAPSHOT-profiled.jar"));
+        Manifest manifest = jarFile.getManifest();
+
+        JarOutputStream dest = new JarOutputStream(new FileOutputStream(outpath+"data-structures-1.0-SNAPSHOT-profiled.jar"),manifest);
+
+        /*ZipEntry e = new ZipEntry("META-INF/");
+        e.setTime(System.currentTimeMillis());
+        e.setSize(0);
+        e.setCrc(0);
+        dest.putNextEntry(e);
+        e = new ZipEntry(MANIFEST_NAME);
+        e.setTime(System.currentTimeMillis());
+
+        dest.putNextEntry(e);
+        manifest.write(dest);
+        dest.closeEntry();*/
+
         processJar(jarFile, dest);
     }
 
@@ -69,11 +92,21 @@ public class AsmReader {
     private static void processJar(JarFile jarFile, JarOutputStream tempJar) throws IOException {
 
         jarFile.stream().forEach(jarEntry -> {
+            try {
+                System.out.println(jarEntry.getName()+" "+jarEntry.getAttributes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            if(jarEntry.isDirectory()) {
+                addDirectoryEntryToJar(jarEntry,tempJar);
+            }
+
             if (!jarEntry.isDirectory() && jarEntry.getName().endsWith(".class")) {
                 InputStream classFileInputStream = getClassFileInputStream(jarFile, jarEntry);
                 processEntry(jarEntry, classFileInputStream, tempJar, jarFile);
             } else {
-                if (!jarEntry.isDirectory()) {
+                if (!jarEntry.isDirectory() && !jarEntry.getName().equals(MANIFEST_NAME)) {
                     copyFile(jarFile, tempJar, jarEntry);
                 }
             }
@@ -83,6 +116,15 @@ public class AsmReader {
         jarFile.close();
     }
 
+    private static void addDirectoryEntryToJar(JarEntry jarEntry, JarOutputStream outputJar) {
+        try {
+            outputJar.putNextEntry(jarEntry);
+            outputJar.closeEntry();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     private static void copyFile(JarFile jarFile, JarOutputStream tempJar, JarEntry jarEntry) {
         try {
@@ -90,7 +132,7 @@ public class AsmReader {
             tempJar.putNextEntry(jarEntry);
             InputStream is = getClassFileInputStream(jarFile, jarEntry);
             tempJar.write(getBytes(is));
-
+            tempJar.closeEntry();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -118,10 +160,22 @@ public class AsmReader {
 
             byte[] b = cw.toByteArray();
             JarEntry outputJarEntry = new JarEntry(jarEntry.getName());
-            outputJarEntry.setSize(b.length);
+            outputJarEntry.setTime(System.currentTimeMillis());
+
+            //need to directory entries as well
+
+            if(b.length == 0) {
+                outputJarEntry.setMethod(ZipEntry.STORED);
+                outputJarEntry.setSize(0);
+                outputJarEntry.setCrc(0);
+            } else {
+                crc32File(outputJarEntry, b);
+            }
+
+            //outputJarEntry.setSize(b.length);
             outputJar.putNextEntry(outputJarEntry);
             outputJar.write(b);
-
+            outputJar.closeEntry();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -136,4 +190,75 @@ public class AsmReader {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Computes the crc32 of a File.  This is necessary when the
+     * ZipOutputStream is in STORED mode.
+     */
+    private static void crc32File(ZipEntry e, byte[] buffer) throws IOException {
+        CRC32OutputStream os = new CRC32OutputStream();
+        os.write(buffer,0,buffer.length);
+        os.updateEntry(e);
+    }
+
+    private static class CRC32OutputStream extends java.io.OutputStream {
+        final CRC32 crc = new CRC32();
+        long n = 0;
+
+        CRC32OutputStream() {}
+
+        public void write(int r) throws IOException {
+            crc.update(r);
+            n++;
+        }
+
+        public void write(byte[] b, int off, int len) throws IOException {
+            crc.update(b, off, len);
+            n += len;
+        }
+
+        /**
+         * Updates a ZipEntry which describes the data read by this
+         * output stream, in STORED mode.
+         */
+        public void updateEntry(ZipEntry e) {
+            e.setMethod(ZipEntry.STORED);
+            e.setSize(n);
+            e.setCrc(crc.getValue());
+        }
+    }
+
+    /**
+     * Generates class index file for the specified root jar file.
+     */
+    /*void genIndex(String rootjar, String[] files) throws IOException {
+        List<String> jars = getJarPath(rootjar);
+        int njars = jars.size();
+        String[] jarfiles;
+
+        if (njars == 1 && files != null) {
+            // no class-path attribute defined in rootjar, will
+            // use command line specified list of jars
+            for (int i = 0; i < files.length; i++) {
+                jars.addAll(getJarPath(files[i]));
+            }
+            njars = jars.size();
+        }
+        jarfiles = jars.toArray(new String[njars]);
+        JarIndex index = new JarIndex(jarfiles);
+        dumpIndex(rootjar, index);
+    }*/
+
+    /*private void addIndex(JarIndex var1, ZipOutputStream var2) throws IOException {
+        ZipEntry indexFile = new ZipEntry("META-INF/INDEX.LIST");
+        indexFile.setTime(System.currentTimeMillis());
+
+        CRC32OutputStream var4 = new CRC32OutputStream();
+        var1.write(var4);
+        var4.updateEntry(indexFile);
+
+        var2.putNextEntry(indexFile);
+        var1.write(var2);
+        var2.closeEntry();
+    }*/
 }
