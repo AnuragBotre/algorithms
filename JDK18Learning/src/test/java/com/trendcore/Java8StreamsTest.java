4174563d04a7edd3c3ec742b6919d9245bcaa78f
anurag <anurag.botre.202@gmail.com>
2020-11-16 13:03:17 +0530

    Java 8 Stream Notes

--- JDK18Learning/src/test/java/com/trendcore/Java8StreamsTest.java
+++ JDK18Learning/src/test/java/com/trendcore/Java8StreamsTest.java
@@ -43,9 +43,9 @@ public class Java8StreamsTest {
                     return secondStreamSupplier
                             .get()
                             .map(
-                            string -> new Tuple2<>(integer, string)
+                                    string -> new Tuple2<>(integer, string)
                             )
-                        ;
+                            ;
                 })
                 .forEach(tuple2 -> System.out.println(tuple2));
 
@@ -61,6 +61,59 @@ public class Java8StreamsTest {
                         .stream()
                         .map(string -> new Tuple2<>(integer, string))
         ).forEach(integerStringTuple2 -> System.out.println(integerStringTuple2));
+    }
+
+    @Test
+    public void limit() {
+        List<Integer> listOfInteger = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+        listOfInteger
+                .stream()
+                .limit(5)
+                .forEach(integer -> System.out.println(integer))
+                ;
+    }
+
+    @Test
+    public void offset() {
+        List<Integer> listOfInteger = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+        listOfInteger
+                .stream()
+                .skip(3)
+                .forEach(integer -> System.out.println(integer))
+        ;
+    }
+
+    @Test
+    public void limitOffset() {
+        List<Integer> listOfInteger = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+        /**
+         * Note order of limit offset is also important.
+         */
+        listOfInteger
+                .stream()
+                .skip(3)
+                .limit(5)
+                .forEach(integer -> System.out.println(integer))
+        ;
+
+
+    }
+
+    @Test
+    public void limitOffsetOrderIsImportant() {
+        List<Integer> listOfInteger = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        /**
+         * This code will limit to 5 elements then it will skip three elements and will show rest of elements.
+         */
+        listOfInteger
+                .stream()
+                .limit(5)
+                .skip(3)
+                .forEach(integer -> System.out.println(integer))
+        ;
     }
 
     private <T1, T2> Object tuple(T1 t1, T2 t2) {
